0b12b1d7a4b427c591ef7114e230b7587e171217
Ömer Ekinci <oekinci.sa@gmail.com>
2024-12-26 13:50:03 +0300

    .

--- components/common/Header/Navigation.tsx
+++ components/common/Header/Navigation.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import Link from "next/link";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from '@radix-ui/react-navigation-menu';
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 
 const Navigation = () => {
   return (
-    <div className='flex gap-8'>
+    <div className="flex gap-8">
+      <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+  
       <Link href="/">Anasayfa</Link>
       <Link href="/hakkimizda">Hakkımızda</Link>
       <Link href="/hisseal">Hisse İşlemleri</Link>

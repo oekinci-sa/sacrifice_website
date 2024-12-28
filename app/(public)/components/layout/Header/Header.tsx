c45d551934f4cf99be59cdb0fedfd333b3cb6ddd
Ömer Ekinci <oekinci.sa@gmail.com>
2024-12-28 15:42:21 +0300

    .

--- app/(public)/components/layout/Header/Header.tsx
+++ app/(public)/components/layout/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Navigation from "./navigation";
 import RightPart from "./rightpart";
-import Logo from "./Logo";
+import Logo from "@/app/(public)/components/layout/Header/logo";
 
 const Header = () => {
   return (

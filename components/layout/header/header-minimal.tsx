159105528a6708ce2a5fd4e995aa0513f9c33ea5
Ömer Ekinci <oekinci.sa@gmail.com>
2025-06-02 15:19:23 +0300

    .

--- components/layout/header/header-minimal.tsx
+++ components/layout/header/header-minimal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Logo from "@/components/layout/footer/logo";
+import Logo from "@/components/layout/header/logo";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
